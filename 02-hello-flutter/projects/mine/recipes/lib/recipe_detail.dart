38f970346a65b218a55831a1bada112571a48b91
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-11-14 20:52:30 -0500

    02-recipes: Completed

---
+++ 02-hello-flutter/projects/mine/recipes/lib/recipe_detail.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({Key? key, required this.recipe}) : super(key: key);
+
+  @override
+  _RecipeDetailState createState() => _RecipeDetailState();
+
+  // Same as above
+  // @override
+  // State<RecipeDetail> createState() {
+  //   return _RecipeDetailState();
+  // }
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  int _sliderVal = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.recipe.label),
+        ),
+        body: SafeArea(
+          child: Column(children: <Widget>[
+            SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image(image: AssetImage(widget.recipe.imageUrl))),
+            const SizedBox(height: 4),
+            Text(
+              widget.recipe.label,
+              style: const TextStyle(fontSize: 18),
+            ),
+            // 7
+            Expanded(
+                // 8
+                child: ListView.builder(
+              padding: const EdgeInsets.all(7.0),
+              itemCount: widget.recipe.ingredients.length,
+              itemBuilder: (BuildContext context, int index) {
+                final ingredient = widget.recipe.ingredients[index];
+                return Text(
+                    '${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}');
+              },
+            )),
+            Slider(
+                min: 1,
+                max: 10,
+                divisions: 9,
+                label: '${_sliderVal * widget.recipe.servings} servings',
+                value: _sliderVal.toDouble(),
+                onChanged: (newValue) {
+                  // newValue is a double
+                  setState(() {
+                    _sliderVal =
+                        newValue.round(); // round() like this returns an int
+                  });
+                },
+                activeColor: Colors.green,
+                inactiveColor: Colors.black)
+          ]),
+        ));
+  }
+}
